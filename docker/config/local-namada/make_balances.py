011183fa8c0e1d7044022aac56fb8f50ddc3aee3
iskay <ikay@lakeheadu.ca>
2024-09-27 18:08:50 +0000

    give faucet balance for other testnet assets

--- docker/config/local-namada/make_balances.py
+++ docker/config/local-namada/make_balances.py
@@ -51,50 +51,72 @@ TOKEN_ALLOCATIONS = {
     'NAM': {
         'FAUCET_AMOUNT':  "499710000",            # FAUCET_AMOUNT
         'ACCOUNT_AMOUNT': f"{self_bond_amount}",  # ACCOUNT_AMOUNT
-        'USER_AMOUNT':    "10000",                 # USER_AMOUNT
+        'STEWARD_AMOUNT': "17710000",             # STEWARD_AMOUNT
+        'USER_AMOUNT':    "10000",                # USER_AMOUNT
         'VAL_AMOUNT':     "1"
     },
     'BTC': {
-        'FAUCET_AMOUNT':  "17710000",             # FAUCET_AMOUNT
-        'ACCOUNT_AMOUNT': "1000000",              # ACCOUNT_AMOUNT
-        'USER_AMOUNT':    "10000"                 # USER_AMOUNT
+        'FAUCET_AMOUNT':  "17710000",
+        'ACCOUNT_AMOUNT': f"{self_bond_amount}",
+        'STEWARD_AMOUNT': "17710000",
     },
-    '___': {
-        'FAUCET_AMOUNT':  "499710000",            # FAUCET_AMOUNT
-        'ACCOUNT_AMOUNT': "100000000",            # ACCOUNT_AMOUNT
-      'USER_AMOUNT':      "10000"                 # USER_AMOUNT
-    }
+    'ETH': {
+        'FAUCET_AMOUNT':  "17710000",
+        'ACCOUNT_AMOUNT': f"{self_bond_amount}",
+        'STEWARD_AMOUNT': "17710000",
+    },
+    'DOT': {
+        'FAUCET_AMOUNT':  "17710000",
+        'ACCOUNT_AMOUNT': f"{self_bond_amount}",
+        'STEWARD_AMOUNT': "17710000",
+    },
+    'Schnitzel': {
+        'FAUCET_AMOUNT':  "17710000",
+        'ACCOUNT_AMOUNT': f"{self_bond_amount}",
+        'STEWARD_AMOUNT': "17710000",
+    },
+    'Apfel': {
+        'FAUCET_AMOUNT':  "17710000",
+        'ACCOUNT_AMOUNT': f"{self_bond_amount}",
+        'STEWARD_AMOUNT': "17710000",
+    },
+    'Kartoffel': {
+        'FAUCET_AMOUNT':  "17710000",
+        'ACCOUNT_AMOUNT': f"{self_bond_amount}",
+        'STEWARD_AMOUNT': "17710000",
+    },
 }
 
 def distribute_balances(output_toml, balances_config):
+    # Add entries for tokens in TOKEN_ALLOCATIONS, if they don't already exist
+    for token in TOKEN_ALLOCATIONS:
+        if token not in output_toml['token']:
+            output_toml['token'][token] = {}
+        
     for entry in balances_config:
         for token in output_toml['token']:
             if token in TOKEN_ALLOCATIONS:
                 token_key = token
             else:
-                token_key = '___'                
+                continue
 
             if entry == 'faucet-1':
                 output_toml['token'][token][balances_config[entry]['address']] = TOKEN_ALLOCATIONS[token_key]['FAUCET_AMOUNT']   # FAUCET_AMOUNT
             elif entry == 'steward-1':
-                output_toml['token'][token][balances_config[entry]['address']] = TOKEN_ALLOCATIONS[token_key]['ACCOUNT_AMOUNT']  # ACCOUNT_AMOUNT
+                output_toml['token'][token][balances_config[entry]['address']] = TOKEN_ALLOCATIONS[token_key]['STEWARD_AMOUNT']  # STEWARD_AMOUNT
 
             elif 'val' in entry:
                 if 'NAM' in token:
                     if output_toml.get('token', {}).get(token, {}).get(balances_config[entry].get('pk')) is not None:
                         output_toml['token'][token][balances_config[entry]['address']] = output_toml['token'][token][balances_config[entry]['pk']]
-                    else:
-                        output_toml['token'][token][balances_config[entry]['pk']] = TOKEN_ALLOCATIONS[token_key]['VAL_AMOUNT'] # VAL_AMOUNT
-                        output_toml['token'][token][balances_config[entry]['address']] = TOKEN_ALLOCATIONS[token_key]['VAL_AMOUNT'] # VAL_AMOUNT
 
             # namada-x validators
             else:
                 if 'NAM' in token:
-                    # output_toml['token'][token][balances_config[entry]['pk']] = TOKEN_ALLOCATIONS[token_key]['ACCOUNT_AMOUNT'] # ACCOUNT_AMOUNT
                     output_toml['token'][token][balances_config[entry]['address']] = TOKEN_ALLOCATIONS[token_key]['ACCOUNT_AMOUNT'] # ACCOUNT_AMOUNT
-                else:
-                    # output_toml['token'][token][balances_config[entry]['pk']] = TOKEN_ALLOCATIONS[token_key]['ACCOUNT_AMOUNT'] # ACCOUNT_AMOUNT
-                    output_toml['token'][token][balances_config[entry]['address']] = TOKEN_ALLOCATIONS[token_key]['ACCOUNT_AMOUNT'] # ACCOUNT_AMOUNT
+
+    # for housefire -- give this address a balance for testing purposes
+    output_toml['token']['NAM']['tnam1qz4u7j7dkxj5wv9xuwy2qemaeeqd450ysgl7pq0r'] = '10000'
     return output_toml
 
 
