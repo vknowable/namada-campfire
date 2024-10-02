8a58652b6291d238efcdae2e1301758dbef8a071
sirouk <8901571+sirouk@users.noreply.github.com>
2024-10-02 02:52:36 +0000

    revert last five accidental commits

--- scripts/launch-faucet-fe.sh
+++ scripts/launch-faucet-fe.sh
@@ -3,7 +3,7 @@
 ### Grab the repo
 rm -rf $HOME/namada-interface
 cd $HOME
-#git clone -b v0.1.0-0e77e71 https://github.com/anoma/namada-interface.git
+
 git clone -b main https://github.com/anoma/namada-interface.git
 
 
@@ -24,25 +24,8 @@ source $HOME/campfire.env
 # write env file
 env_file=$HOME/namada-interface/apps/faucet/.env
 {
-
-    # for v0.1.0-0e77e71: as documented in: namada-campfire/docker/container-build/faucet-frontend/README.md
-    # echo "REACT_APP_FAUCET_API_URL=https://api.faucet.$DOMAIN"
-    # echo "REACT_APP_FAUCET_API_ENDPOINT=/api/v1/faucet"
-    # echo "REACT_APP_FAUCET_LIMIT=1000"
-    # echo "REACT_APP_TOKEN_NAM=$NAM"
-
-    # for main branch:
     echo "NAMADA_INTERFACE_FAUCET_API_URL=https://api.faucet.$DOMAIN"
     echo "NAMADA_INTERFACE_FAUCET_API_ENDPOINT=/api/v1/faucet"
-    # echo "NAMADA_INTERFACE_FAUCET_LIMIT=1000"
-    # echo "NAMADA_INTERFACE_PROXY_PORT=9000"
-    # echo "NAMADA_INTERFACE_NAMADA_TOKEN=$NAM"
-    
-    # # for main branch:
-    # echo "INDEXER_URL=https://indexer.$DOMAIN:443"
-    # echo "RPC_URL=https://rpc.$DOMAIN:443"
-    # echo "CHAIN_ID=$CHAIN_ID"
-
 } > "$env_file"
 
 
--- scripts/launch-interface.sh
+++ scripts/launch-interface.sh
@@ -9,22 +9,17 @@ INTERFACE_DIR="apps/namadillo"
 ### Grab the repo
 rm -rf ~/namada-interface
 cd ~
-#git clone -b v0.1.0-0e77e71 https://github.com/anoma/namada-interface.git
 git clone -b main https://github.com/anoma/namada-interface.git
 
-#cd $HOME/namada-interface && git checkout 1ed4d1285ffbf654c84a80353537023ba98e0614
 cd $HOME/namada-interface && git fetch --all && git checkout main && git pull
 cp -f $HOME/namada-campfire/docker/container-build/namada-interface/Dockerfile $REPO_DIR/Dockerfile-interface
 cp -f $HOME/namada-campfire/docker/container-build/namada-interface/nginx.conf $REPO_DIR/nginx.conf
 
 
-export CAMPFIRE_CHAIN_DATA="$HOME/chaindata/namada-1"
-export CHAIN_DATA=${CHAIN_DATA:-$CAMPFIRE_CHAIN_DATA}
 
-
-export CHAIN_ID=$(awk -F'=' '/default_chain_id/ {gsub(/[ "]/, "", $2); print $2}' "$CHAIN_DATA/global-config.toml")
-export NAM=$(awk '/\[addresses\]/ {found=1} found && /nam = / {gsub(/.*= "/, ""); sub(/"$/, ""); print; exit}' "$CHAIN_DATA/$CHAIN_ID/wallet.toml")
-export FAUCET_ADDRESS=$(awk '/\[addresses\]/ {found=1} found && /faucet-1 = / {gsub(/.*= "/, ""); sub(/"$/, ""); sub(/unencrypted:/, ""); print; exit}' "$CHAIN_DATA/$CHAIN_ID/wallet.toml")
+export CHAIN_ID=$(awk -F'=' '/default_chain_id/ {gsub(/[ "]/, "", $2); print $2}' "$HOME/chaindata/namada-1/global-config.toml")
+export NAM=$(awk '/\[addresses\]/ {found=1} found && /nam = / {gsub(/.*= "/, ""); sub(/"$/, ""); print; exit}' "$HOME/chaindata/namada-1/$CHAIN_ID/wallet.toml")
+export FAUCET_ADDRESS=$(awk '/\[addresses\]/ {found=1} found && /faucet-1 = / {gsub(/.*= "/, ""); sub(/"$/, ""); sub(/unencrypted:/, ""); print; exit}' "$HOME/chaindata/namada-1/$CHAIN_ID/wallet.toml")
 
 source $HOME/campfire.env
 
@@ -47,10 +42,8 @@ env_file="$REPO_DIR/$INTERFACE_DIR/.env"
 
 } > "$env_file"
 
-# This was the template file for Namadillo config.
-#cp -f $REPO_DIR/docker/.namadillo.config.toml $REPO_DIR/docker/namadillo.config.toml
 
-# This is the template file for Namadillo config.
+# This is a template file for Namadillo config.
 # write config file
 config_file="$REPO_DIR/$INTERFACE_DIR/public/config.toml"
 {
